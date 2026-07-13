2b0890194021ca3fd5201da294e91b1d0ee2b300
aymanmahrous <70751313+aymanmahrous@users.noreply.github.com>
2026-07-14 00:28:42 +0400

    fix: serve production sitemap through app route

---
+++ src/routes/sitemap[.]xml.ts
@@ -0,0 +1,35 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+const SITEMAP_XML = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:xhtml="http://www.w3.org/1999/xhtml">
+  <url>
+    <loc>https://www.relaxfixuae.com/</loc>
+    <lastmod>2026-07-10</lastmod>
+    <xhtml:link rel="alternate" hreflang="ar-AE" href="https://www.relaxfixuae.com/" />
+    <xhtml:link rel="alternate" hreflang="en-AE" href="https://www.relaxfixuae.com/en" />
+    <xhtml:link rel="alternate" hreflang="x-default" href="https://www.relaxfixuae.com/" />
+  </url>
+  <url>
+    <loc>https://www.relaxfixuae.com/en</loc>
+    <lastmod>2026-07-10</lastmod>
+    <xhtml:link rel="alternate" hreflang="ar-AE" href="https://www.relaxfixuae.com/" />
+    <xhtml:link rel="alternate" hreflang="en-AE" href="https://www.relaxfixuae.com/en" />
+    <xhtml:link rel="alternate" hreflang="x-default" href="https://www.relaxfixuae.com/" />
+  </url>
+</urlset>`;
+
+export const Route = createFileRoute("/sitemap.xml")({
+  server: {
+    handlers: {
+      GET: async () =>
+        new Response(SITEMAP_XML, {
+          status: 200,
+          headers: {
+            "Content-Type": "application/xml; charset=utf-8",
+            "Cache-Control": "public, max-age=300, s-maxage=3600",
+          },
+        }),
+    },
+  },
+});
