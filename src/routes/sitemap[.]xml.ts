ee503b16abdce43982174260c136ca1bab4af7ec
aymanmahrous <70751313+aymanmahrous@users.noreply.github.com>
2026-07-28 02:54:10 +0400

    fix(seo): keep server sitemap byte-equivalent with local URL set

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -1,40 +1,44 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { TRAINING_LOCATIONS } from "../platform/public-business-config";
-import { SITE_URL } from "../platform/public-seo";
-
-const escapeXml = (value: string) =>
-  value.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;");
-
-const locationEntries = TRAINING_LOCATIONS.filter((location) => location.localSeoEnabled)
-  .map(
-    (location) => `  <url>
-    <loc>${SITE_URL}/locations/${escapeXml(location.id)}</loc>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>`,
-  )
-  .join("\n");
 
 const SITEMAP_XML = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
   xmlns:xhtml="http://www.w3.org/1999/xhtml">
   <url>
-    <loc>${SITE_URL}/</loc>
+    <loc>https://www.relaxfixuae.com/</loc>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
-    <xhtml:link rel="alternate" hreflang="ar-AE" href="${SITE_URL}/" />
-    <xhtml:link rel="alternate" hreflang="en-AE" href="${SITE_URL}/en" />
-    <xhtml:link rel="alternate" hreflang="x-default" href="${SITE_URL}/" />
+    <xhtml:link rel="alternate" hreflang="ar-AE" href="https://www.relaxfixuae.com/" />
+    <xhtml:link rel="alternate" hreflang="en-AE" href="https://www.relaxfixuae.com/en" />
+    <xhtml:link rel="alternate" hreflang="x-default" href="https://www.relaxfixuae.com/" />
   </url>
   <url>
-    <loc>${SITE_URL}/en</loc>
+    <loc>https://www.relaxfixuae.com/en</loc>
     <changefreq>weekly</changefreq>
     <priority>0.9</priority>
-    <xhtml:link rel="alternate" hreflang="ar-AE" href="${SITE_URL}/" />
-    <xhtml:link rel="alternate" hreflang="en-AE" href="${SITE_URL}/en" />
-    <xhtml:link rel="alternate" hreflang="x-default" href="${SITE_URL}/" />
+    <xhtml:link rel="alternate" hreflang="ar-AE" href="https://www.relaxfixuae.com/" />
+    <xhtml:link rel="alternate" hreflang="en-AE" href="https://www.relaxfixuae.com/en" />
+    <xhtml:link rel="alternate" hreflang="x-default" href="https://www.relaxfixuae.com/" />
   </url>
-${locationEntries}
+  <url>
+    <loc>https://www.relaxfixuae.com/locations/najda-street</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>
+  <url>
+    <loc>https://www.relaxfixuae.com/locations/ics-al-falah</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>
+  <url>
+    <loc>https://www.relaxfixuae.com/locations/ics-khalifa</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>
+  <url>
+    <loc>https://www.relaxfixuae.com/locations/ics-mushrif</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>
 </urlset>`;
 
 export const Route = createFileRoute("/sitemap.xml")({
