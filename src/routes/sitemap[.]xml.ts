496c11c412f29428b8334ad76c19a5e6a9291201
aymanmahrous <70751313+aymanmahrous@users.noreply.github.com>
2026-07-28 02:48:42 +0400

    feat(seo): expand sitemap with Abu Dhabi local location entries

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -1,20 +1,40 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { TRAINING_LOCATIONS } from "../platform/public-business-config";
+import { SITE_URL } from "../platform/public-seo";
+
+const escapeXml = (value: string) =>
+  value.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;");
+
+const locationEntries = TRAINING_LOCATIONS.filter((location) => location.localSeoEnabled)
+  .map(
+    (location) => `  <url>
+    <loc>${SITE_URL}/locations/${escapeXml(location.id)}</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>`,
+  )
+  .join("\n");
 
 const SITEMAP_XML = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
   xmlns:xhtml="http://www.w3.org/1999/xhtml">
   <url>
-    <loc>https://www.relaxfixuae.com/</loc>
-    <xhtml:link rel="alternate" hreflang="ar-AE" href="https://www.relaxfixuae.com/" />
-    <xhtml:link rel="alternate" hreflang="en-AE" href="https://www.relaxfixuae.com/en" />
-    <xhtml:link rel="alternate" hreflang="x-default" href="https://www.relaxfixuae.com/" />
+    <loc>${SITE_URL}/</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+    <xhtml:link rel="alternate" hreflang="ar-AE" href="${SITE_URL}/" />
+    <xhtml:link rel="alternate" hreflang="en-AE" href="${SITE_URL}/en" />
+    <xhtml:link rel="alternate" hreflang="x-default" href="${SITE_URL}/" />
   </url>
   <url>
-    <loc>https://www.relaxfixuae.com/en</loc>
-    <xhtml:link rel="alternate" hreflang="ar-AE" href="https://www.relaxfixuae.com/" />
-    <xhtml:link rel="alternate" hreflang="en-AE" href="https://www.relaxfixuae.com/en" />
-    <xhtml:link rel="alternate" hreflang="x-default" href="https://www.relaxfixuae.com/" />
+    <loc>${SITE_URL}/en</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+    <xhtml:link rel="alternate" hreflang="ar-AE" href="${SITE_URL}/" />
+    <xhtml:link rel="alternate" hreflang="en-AE" href="${SITE_URL}/en" />
+    <xhtml:link rel="alternate" hreflang="x-default" href="${SITE_URL}/" />
   </url>
+${locationEntries}
 </urlset>`;
 
 export const Route = createFileRoute("/sitemap.xml")({
